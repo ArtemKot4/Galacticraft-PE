15a0e82d3087c4eaf15d215d43f93021966c93ca
Artem0n4 <artemon4xxl@gmail.com>
2024-01-29 07:55:55 +0300

    что-то с покет гитом

--- dev/Galacticraft/Items/battery.ts
+++ dev/Galacticraft/Items/battery.ts
@@ -5,7 +5,7 @@ custom?: string): void {
   const item = new GItem(id, 1).info(
     (typeof count === "number") ? (item, name) => 
      name + "\n" + "gJ: " + item.data + " / " + count : 
-     (item, name) => name + "\n§7" + (("Infinity" + "§6⚡") ?? custom),
+     (item, name) => name + "\n§7" + (custom ?? ("Infinity" + "§6⚡")),
 ﻿);
     (count == "infinite") ? battery.setInfinite(ItemID[id], strength || 0) : 
  ChargeItemRegistry.registerItem(ItemID[id], "GalacticraftJoule",
